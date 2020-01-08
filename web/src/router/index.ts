f796b53075b8dcb6b12a88600b03a5ff736f958f
krnan9525 <sepyng@gmail.com>
2020-01-08 23:35:41 +0000

    change route name

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
-import CallLogs from '../views/CallLogs.vue';
 
 Vue.use(VueRouter);
 
@@ -12,7 +11,7 @@ const routes = [
     component: Home
   },
   {
-    path: '/call-logs',
+    path: '#call-logs',
     name: 'call logs',
     component: () =>
       import(/* webpackChunkName: "call-logs" */ '../views/CallLogs.vue')
