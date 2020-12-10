7f4dcbfcb21209577807c71c1b8786b95e3e1b7a
Maksym Shykula <mshykula@twilio.com>
2020-12-10 16:32:31 +0000

    Add Phone Numbers API

--- api/server/api/controllers/phoneNumbers/controller.ts
+++ api/server/api/controllers/phoneNumbers/controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import PhoneNumbersService from '../../services/phoneNumbers.service';
+import PhoneNumbersService, { PhoneNumberType } from '../../services/phoneNumbers.service';
 
 export class PhoneNumbersController {
   getAvailableCountries(req: Request, res: Response, next: NextFunction): void {
@@ -8,6 +8,17 @@ export class PhoneNumbersController {
     PhoneNumbersService.getAvailableCountries(accountSid, accessToken)
       .then(countries => {
         res.json(countries);
+      })
+      .catch(next);
+  }
+
+  getAvailableNumbers(req: Request, res: Response, next: NextFunction): void {
+    const { accountSid, accessToken } = req.query;
+    const { country, type } = req.params as {country: string, type: PhoneNumberType};
+
+    PhoneNumbersService.getAvailableNumbers(accountSid, accessToken, country, type)
+      .then(numbers => {
+        res.json(numbers);
       })
       .catch(next);
   }
